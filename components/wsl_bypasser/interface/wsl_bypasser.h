f5b34e2a0d463571ac5f74e19cb3156a749b3361
SameerAlSahab <jahirmondol5456@gmail.com>
2025-08-10 13:20:02 +0600

    Update wsl_bypasser.h

--- components/wsl_bypasser/interface/wsl_bypasser.h
+++ components/wsl_bypasser/interface/wsl_bypasser.h
@@ -31,4 +31,7 @@ void wsl_bypasser_send_raw_frame(const uint8_t *frame_buffer, int size);
  */
 void wsl_bypasser_send_deauth_frame(const wifi_ap_record_t *ap_record);
 
+// Add new function prototype
+void wsl_bypasser_send_beacon_frame(uint8_t *bssid, uint8_t *ssid, uint8_t ssid_length, uint8_t channel);
+
 #endif
