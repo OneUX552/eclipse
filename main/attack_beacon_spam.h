0181432413a3b4ecbf3ade8e197304a3f3d349c1
SameerAlSahab <jahirmondol5456@gmail.com>
2025-08-10 15:25:33 +0600

    Update attack_beacon_spam.h

--- main/attack_beacon_spam.h
+++ main/attack_beacon_spam.h
@@ -4,17 +4,19 @@
 #include "attack.h"
 
 /**
- * @brief Starts Beacon Spam attack.
+ * @brief Starts BLE Beacon Spam attack.
  * 
- * This attack floods the area with fake WiFi networks to disrupt WiFi scanning.
+ * This attack sends BLE advertising packets simulating iBeacon spam
+ * for a specified duration.
  * 
- * @param beacon_count Number of fake networks to create
+ * @param duration_seconds Duration of the attack in seconds.
  */
-void attack_beacon_spam_start(uint8_t beacon_count);
+void attack_beacon_spam_start(int duration_seconds);
 
 /**
- * @brief Stops Beacon Spam attack.
+ * @brief Stops BLE Beacon Spam attack.
  */
-void attack_beacon_spam_stop();
+void attack_beacon_spam_stop(void);
 
-#endif
+#endif // ATTACK_BEACON_SPAM_H
+
