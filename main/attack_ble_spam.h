2e51276a2bd1d5c306d24e4ecce38d48bab76284
Jisan <166594898+OneUX552@users.noreply.github.com>
2025-08-11 20:46:57 +0600

    Update and rename attack_ble_beacon.h to attack_ble_spam.h

---
+++ main/attack_ble_spam.h
@@ -0,0 +1,16 @@
+#ifndef ATTACK_BLE_SPAM_H
+#define ATTACK_BLE_SPAM_H
+
+#include "attack.h"
+
+/**
+ * @brief Starts BLE spam attack
+ */
+void attack_ble_spam_start();
+
+/**
+ * @brief Stops BLE spam attack
+ */
+void attack_ble_spam_stop();
+
+#endif
